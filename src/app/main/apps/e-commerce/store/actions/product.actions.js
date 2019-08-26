1fd61ed815a029337ef5714ba2500420a414619a
Paul IK <makvie@yahoo.ca>
2019-08-26 19:38:07 -0400

    Updated code for util, product, about

--- src/app/main/apps/e-commerce/store/actions/product.actions.js
+++ src/app/main/apps/e-commerce/store/actions/product.actions.js
@@ -1,66 +1,78 @@
-import axios from 'axios';
-import {FuseUtils} from '@fuse';
-import {showMessage} from 'app/store/actions/fuse';
+import axios from "axios";
+import { FuseUtils } from "@fuse";
+import { showMessage } from "app/store/actions/fuse";
 
-export const GET_PRODUCT = '[E-COMMERCE APP] GET PRODUCT';
-export const SAVE_PRODUCT = '[E-COMMERCE APP] SAVE PRODUCT';
+// Type of Actions
+export const GET_PRODUCT = "[E-COMMERCE APP] GET PRODUCT";
+export const SAVE_PRODUCT = "[E-COMMERCE APP] SAVE PRODUCT";
+export const PUBLISH_PRODUCT = "[E-COMMERCE APP] PUBLISH PRODUCT";
+export const REMOVE_PRODUCT = "[E-COMMERCE APP] REMOVE PRODUCT";
 
-export function getProduct(params)
-{
-    const request = axios.get('/api/e-commerce-app/product', {params});
+// Get Actions
+export function getProduct(params) {
+  const request = axios.get("/api/e-commerce-app/product", { params });
 
-    return (dispatch) =>
-        request.then((response) =>
-            dispatch({
-                type   : GET_PRODUCT,
-                payload: response.data
-            })
-        );
+  return dispatch =>
+    request.then(response =>
+      dispatch({
+        type: GET_PRODUCT,
+        payload: response.data
+      })
+    );
 }
 
-export function saveProduct(data)
-{
-    const request = axios.post('/api/e-commerce-app/product/save', data);
+// Save Product
+export function saveProduct(data) {
+  const request = axios.post("/api/e-commerce-app/product/save", data);
 
-    return (dispatch) =>
-        request.then((response) => {
+  return dispatch =>
+    request.then(response => {
+      dispatch(showMessage({ message: "Product Saved" }));
 
-                dispatch(showMessage({message: 'Product Saved'}));
-
-                return dispatch({
-                    type   : SAVE_PRODUCT,
-                    payload: response.data
-                })
-            }
-        );
+      return dispatch({
+        type: SAVE_PRODUCT,
+        payload: response.data
+      });
+    });
 }
 
-export function newProduct()
-{
-    const data = {
-        id              : FuseUtils.generateGUID(),
-        name            : '',
-        handle          : '',
-        description     : '',
-        categories      : [],
-        tags            : [],
-        images          : [],
-        priceTaxExcl    : 0,
-        priceTaxIncl    : 0,
-        taxRate         : 0,
-        comparedPrice   : 0,
-        quantity        : 0,
-        sku             : '',
-        width           : '',
-        height          : '',
-        depth           : '',
-        weight          : '',
-        extraShippingFee: 0,
-        active          : true
-    };
+export function removeProduct({ params }) {}
 
-    return {
-        type   : GET_PRODUCT,
-        payload: data
-    }
+export function publishProduct({ params }) {}
+
+// New Product
+// TODO: Make this for each type of Product (Cloth, Shoe, Accessosaries)
+/*
+
+isPublished: false;
+isSave: fales;
+
+*/
+export function newProduct() {
+  const data = {
+    id: FuseUtils.generateGUID(),
+    name: "",
+    handle: "",
+    description: "",
+    categories: [],
+    tags: [],
+    images: [],
+    priceTaxExcl: 0,
+    priceTaxIncl: 0,
+    taxRate: 0,
+    comparedPrice: 0,
+    quantity: 0,
+    sku: "",
+    width: "",
+    height: "",
+    depth: "",
+    weight: "",
+    extraShippingFee: 0,
+    active: true
+  };
+
+  return {
+    type: GET_PRODUCT,
+    payload: data
+  };
 }
